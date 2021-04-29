9ccc5e7d9565cb9f5a8ba17404e37ce79920e5e6
Rainbow120 <shreya.sahay012@gmail.com>
2021-04-29 16:23:32 +0530

    First Commit

---
+++ sketch.js
@@ -0,0 +1,66 @@
+var dog, dogHappy, dogSad;
+var db, foodS, foodStock;
+var fedTime, lastFed, feed, addFood, foodObj;
+
+function preload(){
+    dogImg = loadImage("images/dogImg.png");
+    dogImg2 = loadImage("images/dogImg1.png");
+}
+function setup() {
+  createCanvas(1000, 500);
+  foodObj = new Food();
+
+  db = firebase.database();
+  dog = createSprite(800, 250, 10, 10);
+  dog.addImage(dogImg);
+  dog.scale = 0.3
+
+  feed = createButton("FEED");
+  feed.position(1100, 50);
+  feed.mousePressed(feedDog);
+
+  addFood = createButton("ADD FOOD");
+  addFood.position(1200, 50);
+  addFood.mousePressed(addFood);
+
+foodStock = db.ref('Food');
+foodStock.on("value", readStock);
+}
+
+function draw() {  
+  background(140, 210, 144);
+foodObj.display();
+
+textSize(20);
+fill("white");
+fedTime = db.ref('fedTime');
+fedTime.on('value', function(data){
+  lastFed = data.val();
+})
+if(lastFed >=12){
+  text("LAST FEED :" + lastFed % 12 + 'pm', 350, 30);
+} else if(lastFed === 0){
+  text("LAST FEED : 12 am", 350, 30);
+}else {
+  text("LAST FEED :"+ lastFed+'am', 350, 30);
+}
+  drawSprites();
+  
+} 
+function readStock(data){
+  foodS = data.val();
+  foodObj.updateFoodStock(foodS)
+}
+function feedDog(){
+  dog.addImage(dogImg2)
+  foodObj.updateFoodStock(foodObj.getFoodStock()-1)
+  db.ref('/').update({
+    Food:foodObj.getFoodStock(), fedTime:hour()
+  })
+}
+function addFood(){
+  foodS++
+  db.ref('/').update({
+    Food:foodS
+  })
+}
